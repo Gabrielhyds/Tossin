23ec1fdf9345f25d1b5331d5ef867f3eccaa87f2
Gabrielhyds <starkstory6@gmail.com>
2024-11-25 23:29:29 -0300

    [8] - change cart and add images

--- static/js/app.js
+++ static/js/app.js
@@ -15,19 +15,19 @@ class Produto {
 
 //vetor produtos, do tipo objeto
 const produtos = [
-    new Produto('1','cueca','7896924303364','ZIMBA', 35.00, 'Cueca Tamanho G','Conforto e estilo se encontram na Cueca Zorba, uma peça essencial para o guarda-roupa masculino. Feita com um tecido de alta qualidade, ela proporciona uma sensação suave contra a pele, garantindo conforto durante todo o dia. Seu design moderno e corte perfeito oferecem um ajuste ideal, valorizando a silhueta sem abrir mão da liberdade de movimentos.', 'static/img/cueca_02_branco.webp'),    
-    new Produto('2','meia','3784378583749','Meia Cano Curto Esportiva Puma', 40.00, 'Meia Tamanho G','68% Algodão 27% Poliamida 03% Elastano 02% Outras Fibras','Foto da meia 07'),
-    new Produto('3','cueca','7899303536759','Aorba', 35.00, 'Cueca Tamanho G','A Cueca ComfortFit é a escolha ideal para quem valoriza conforto sem abrir mão do estilo. Feita com um tecido de algodão premium, ela proporciona uma sensação suave contra a pele, tornando-a perfeita para o uso diário. O design anatômico se adapta perfeitamente ao corpo, garantindo que você se sinta seguro e confortável em qualquer situação. Confeccionadas em Algodão e Poliester sem costuras marcantes. Garantem maior conforto e liberdade de movimento. Modelagem moderna e confortável.','static/img/c5.jpg'),
-    new Produto('4','cueca','7892314748797','Cueca Boxer Algodão Sem Costura Zorba', 35.00, 'Cueca Tamanho G','Possuem proteção contra bactérias e maus odores. Feitas com tecnologia MaxFresh, que regula a sensação térmica.','static/img/cueca_preta.png'),
-    new Produto('5','cueca','7898493877544','Cueca Lupo AM Boxer Urban', 35.00, 'Cueca Tamanho G','A opção ideal para os homens que valorizam estilo e bem-estar. A Cueca Boxer Adulto Sem Costura possui fios sintéticos fabricados com filamentos individuais extremamente finos, que garantem uma peça de secagem rápida, macia e durável. Além disso, a tecnologia sem costura evita atritos e incômodos durante o uso, permitindo ajuste perfeito ao corpo, segurança e liberdade de movimento, sempre com muito conforto, qualidade e durabilidade.','Foto_da_cueca_04'),
-    new Produto('6','cueca','7894336754612','Cueca Boxer sem Costura Puma',35.00,'Cueca Tamanho G','Sinta- se confortável em todos os momentos, com esse Kit de Cuecas Boxer sem Costura Puma. Confeccionado em materiais leves e resistentes, proporcionando leveza e maior liberdade dos movimentos. Já o cós em elástico possibilita um ajuste personalizado. Não perca a oportunidade e adquira já seu kit com toda qualidade que a Puma tem a oferecer. Compre já!','Foto_da_cueca_05'),
-    new Produto('7','cueca','7896431264837','Cueca Box Boxer Lupo Sem Costura Basic Microfibra', 35.00,'Cueca Tamanho G','Cueca boxer masculina Lupo. É composto por cuecas masculinas para adultos, modelo boxer confeccionado em microfibra da marca Lupo. Cueca boxer para homens que gostam de estilo. Com formato anatômico, muito mais conforto, malha com elasticidade para ajuste perfeito. Para garantir leveza e suavidade a cada movimento, invista no bem-estar que a cueca sem costura proporciona no dia a dia, escolha certa para acompanhar homens modernos e sofisticados.','Foto_da_cueca_06'),
-    new Produto('8','meia', '3756343564354','Meia Curta Adulto Walk Lupo', 40.00,'Meia Tamanho G','Se você quer meias que sejam confortáveis, bonitas e duráveis, você vai adorar a Meia Curta Adulto Walk Lupo. Ela é ideal para o seu cotidiano, pois suas peças são feitas com um tecido macio e resistente, que se ajusta ao seu pé e não causa irritações na pele. As meias são curtas, ótimas para usar com calçados baixos ou esportivos. As cores são variadas e modernas, combinando com qualquer roupa. E o melhor de tudo: não tem costura, garantindo ainda mais conforto e bem-estar.','Foto da meia 01'),
-    new Produto('10','meia', '3792183356564','Meia Lupo AU Sport Lupo K3', 50.00, 'Meia Tamanho G', 'Qualidade e conforto todos os dias. Cada meia do kit com três unidades possui cano médio e calcanhar verdadeiro, proporcionando ajuste perfeito e conforto superior. Produzida com algodão penteado, tem toque macio, suave, leve e oferece excelente respirabilidade da pele. Polivalente, é perfeita para fazer atividade física e usar no trabalho', 'Foto da meia 02'),
-    new Produto('9','meia', '3712872948598','Meia Olympikus Invisível', 40.00, 'Meia Tamanho G','A Meia Olympikus Invisível é a opção perfeita para seu conforto e praticidade no dia a dia. - Veste do tamanho 39 ao 43. Composição: 62% algodão, 36% poliéster, 2% elastano.', 'Foto da meia 03'),
-    new Produto('10','meia','3721839874672','Meia Adulto Média Esportiva Walk Lupo', 40.00, 'Meia Tamanho M','Se você está em busca de um item que te acompanhe em todos os momentos de prática de esportes, então precisa conhecer a nossa meia adulto média esportiva Walk Lupo. As nossas peças oferecem muito conforto e garante que os movimentos sejam feitos com tranquilidade, além de poderem ser utilizadas com diferentes tipos de calçados esportivos.','Foto da meia 04'),
-    new Produto('11','meia','3789982758374','Meia Nike Sportswear Everyday Essential Unissex', 60.00, 'Meia Tamanho G','As meias Nike Sportswear Everyday Essential são feitas com tecido macio e elástico que se molda ao seu pé para proporcionar conforto o dia todo. Este produto é feito com, no mínimo, 75% de fibras de poliéster reciclado.','Fot da meia 05'),
-    new Produto('12','meia','3782694828678','Meia Nike Elite Unissex', 90.00, 'Meia Tamanho G','A Meia Nike Elite NBA Crew é a escolha do jogador que busca conforto e suporte. Ela é feita com fios de knit premium e tecnologia que absorve o suor, além de amortecimento onde mais importa. A meia contém o logo da NBA e é marcadas verticalmente nas costas com a faixa Elite de primeira linha da Nike Basketball.', 'Foto da meia 06'),
+    new Produto('1','cueca','7896924303364','ZIMBA', 35.00, 'Cueca Tamanho G','Conforto e estilo se encontram na Cueca Zorba, uma peça essencial para o guarda-roupa masculino. Feita com um tecido de alta qualidade, ela proporciona uma sensação suave contra a pele, garantindo conforto durante todo o dia. Seu design moderno e corte perfeito oferecem um ajuste ideal, valorizando a silhueta sem abrir mão da liberdade de movimentos.', 'static/img/cueca_02_branco.webp'),    //
+    new Produto('2','meia','3784378583749','Meia Cano Curto Esportiva Puma', 40.00, 'Meia Tamanho G','68% Algodão 27% Poliamida 03% Elastano 02% Outras Fibras','static/img/meia_07_01.webp'), //
+    new Produto('3','cueca','7899303536759','Aorba', 35.00, 'Cueca Tamanho G','A Cueca ComfortFit é a escolha ideal para quem valoriza conforto sem abrir mão do estilo. Feita com um tecido de algodão premium, ela proporciona uma sensação suave contra a pele, tornando-a perfeita para o uso diário. O design anatômico se adapta perfeitamente ao corpo, garantindo que você se sinta seguro e confortável em qualquer situação. Confeccionadas em Algodão e Poliester sem costuras marcantes. Garantem maior conforto e liberdade de movimento. Modelagem moderna e confortável.','static/img/c5.jpg'), //
+    new Produto('4','cueca','7892314748797','Cueca Boxer Algodão Sem Costura Zorba', 35.00, 'Cueca Tamanho G','Possuem proteção contra bactérias e maus odores. Feitas com tecnologia MaxFresh, que regula a sensação térmica.','static/img/cueca_preta.png'), //
+    new Produto('5','cueca','7898493877544','Cueca Lupo AM Boxer Urban', 35.00, 'Cueca Tamanho G','A opção ideal para os homens que valorizam estilo e bem-estar. A Cueca Boxer Adulto Sem Costura possui fios sintéticos fabricados com filamentos individuais extremamente finos, que garantem uma peça de secagem rápida, macia e durável. Além disso, a tecnologia sem costura evita atritos e incômodos durante o uso, permitindo ajuste perfeito ao corpo, segurança e liberdade de movimento, sempre com muito conforto, qualidade e durabilidade.','static/img/cueca_04_1.png'), //
+    new Produto('6','cueca','7894336754612','Cueca Boxer sem Costura Puma',35.00,'Cueca Tamanho G','Sinta- se confortável em todos os momentos, com esse Kit de Cuecas Boxer sem Costura Puma. Confeccionado em materiais leves e resistentes, proporcionando leveza e maior liberdade dos movimentos. Já o cós em elástico possibilita um ajuste personalizado. Não perca a oportunidade e adquira já seu kit com toda qualidade que a Puma tem a oferecer. Compre já!','static/img/cueca_05_1.png'), //
+    new Produto('7','cueca','7896431264837','Cueca Box Boxer Lupo Sem Costura Basic Microfibra', 35.00,'Cueca Tamanho G','Cueca boxer masculina Lupo. É composto por cuecas masculinas para adultos, modelo boxer confeccionado em microfibra da marca Lupo. Cueca boxer para homens que gostam de estilo. Com formato anatômico, muito mais conforto, malha com elasticidade para ajuste perfeito. Para garantir leveza e suavidade a cada movimento, invista no bem-estar que a cueca sem costura proporciona no dia a dia, escolha certa para acompanhar homens modernos e sofisticados.','static/img/cueca_06_01.webp'), //
+    new Produto('8','meia', '3756343564354','Meia Curta Adulto Walk Lupo', 40.00,'Meia Tamanho G','Se você quer meias que sejam confortáveis, bonitas e duráveis, você vai adorar a Meia Curta Adulto Walk Lupo. Ela é ideal para o seu cotidiano, pois suas peças são feitas com um tecido macio e resistente, que se ajusta ao seu pé e não causa irritações na pele. As meias são curtas, ótimas para usar com calçados baixos ou esportivos. As cores são variadas e modernas, combinando com qualquer roupa. E o melhor de tudo: não tem costura, garantindo ainda mais conforto e bem-estar.','static/img/meia_05_02.png'), //
+    new Produto('10','meia', '3792183356564','Meia Lupo AU Sport Lupo K3', 50.00, 'Meia Tamanho G', 'Qualidade e conforto todos os dias. Cada meia do kit com três unidades possui cano médio e calcanhar verdadeiro, proporcionando ajuste perfeito e conforto superior. Produzida com algodão penteado, tem toque macio, suave, leve e oferece excelente respirabilidade da pele. Polivalente, é perfeita para fazer atividade física e usar no trabalho', 'static/img/meia_03.webp'), //
+    new Produto('9','meia', '3712872948598','Meia Olympikus Invisível', 40.00, 'Meia Tamanho G','A Meia Olympikus Invisível é a opção perfeita para seu conforto e praticidade no dia a dia. - Veste do tamanho 39 ao 43. Composição: 62% algodão, 36% poliéster, 2% elastano.', 'static/img/meia_04.png'), //
+    new Produto('10','meia','3721839874672','Meia Adulto Média Esportiva Walk Lupo', 40.00, 'Meia Tamanho M','Se você está em busca de um item que te acompanhe em todos os momentos de prática de esportes, então precisa conhecer a nossa meia adulto média esportiva Walk Lupo. As nossas peças oferecem muito conforto e garante que os movimentos sejam feitos com tranquilidade, além de poderem ser utilizadas com diferentes tipos de calçados esportivos.','static/img/meia_05_01.png'), //
+    new Produto('11','meia','3789982758374','Meia Nike Sportswear Everyday Essential Unissex', 60.00, 'Meia Tamanho G','As meias Nike Sportswear Everyday Essential são feitas com tecido macio e elástico que se molda ao seu pé para proporcionar conforto o dia todo. Este produto é feito com, no mínimo, 75% de fibras de poliéster reciclado.','static/img/meia_05_02.png'), //
+    new Produto('12','meia','3782694828678','Meia Nike Elite Unissex', 90.00, 'Meia Tamanho G','A Meia Nike Elite NBA Crew é a escolha do jogador que busca conforto e suporte. Ela é feita com fios de knit premium e tecnologia que absorve o suor, além de amortecimento onde mais importa. A meia contém o logo da NBA e é marcadas verticalmente nas costas com a faixa Elite de primeira linha da Nike Basketball.', 'static/img/meia_06_01.png'), //
     ];
 
 //função para ordenar os produtos usamos o 
@@ -51,26 +51,30 @@ function bubbleSortProdutos(arr) {
 
 
 let tipoSelecionado = "Todos"; // Valor padrão
-// Função para buscar o produto com o que o usuário digitar
 function buscarProduto() {
-       // Valor da busca inserido pelo cliente
-       const nomeBusca = document.getElementById('searchInput').value;
+    // Valor da busca inserido pelo cliente
+    const nomeBusca = document.getElementById('searchInput').value.trim();
 
-       // Chama a função bubbleSort para ordenar os produtos
-       bubbleSortProdutos(produtos);
-   
-       // Filtra produtos com base no tipo selecionado e no nome digitado
-       const resultados = produtos.filter(produto => {
-           // Verifica se o produto pertence ao tipo selecionado (ou a todos) e se o nome corresponde
-           const pertenceAoTipo = tipoSelecionado === "Todos" || produto.tipo.toLowerCase() === tipoSelecionado.toLowerCase();
-           const correspondeAoNome = produto.nome.toLowerCase().includes(nomeBusca.toLowerCase());
-           return pertenceAoTipo && correspondeAoNome;
-       });
-   
-       // Exibe os produtos filtrados na interface
-       exibirProdutos(resultados);
+     // Chama a função bubbleSort para ordenar os produtos
+     bubbleSortProdutos(produtos);
+
+    // Filtra produtos com base no tipo selecionado e no nome digitado
+    const resultados = produtos.filter(produto => {
+        // Verifica se o produto pertence ao tipo selecionado (ou a todos) e se o nome corresponde
+        const pertenceAoTipo = tipoSelecionado === "Todos" || produto.tipo.toLowerCase() === tipoSelecionado.toLowerCase();
+        const correspondeAoNome = produto.nome.toLowerCase().includes(nomeBusca.toLowerCase());
+        return pertenceAoTipo && correspondeAoNome;
+    });
+
+
+   // Se a busca estiver vazia, restaura o vetor original
+   if (nomeBusca === null) {
+    exibirProdutos(produtos); // Exibe os produtos na ordem original
+    return;
 }
-
+    // Exibe os produtos filtrados na interface
+    exibirProdutos(resultados);
+}
 
 //função para exibirProdutos na interface
 function exibirProdutos(produtos) {
@@ -99,10 +103,18 @@ function exibirProdutos(produtos) {
         const productCard = `
         <div class="col mb-5">
             <div class="card h-100">
-                <img class="card-img-top" src="${produto.imagem}" alt="${produto.nome}">
+                <img class="card-img-top" src="${produto.imagem}" alt="${produto.nome}" >
                 <div class="card-body p-4 text-center">
                     <h5 class="fw-bolder">${produto.nome}</h5>
                     <p>R$${produto.preco.toFixed(2)}</p>
+                     <div class="rating">
+                        <i class="fa fa-star" style="color:#ffc107"></i>
+                        <i class="fa fa-star" style="color:#ffc107"></i>
+                        <i class="fa fa-star" style="color:#ffc107"></i>
+                         <i class="fa fa-star" style="color:#ffc107"></i>     
+                        <i class="fa fa-star-half-alt" style="color:#ffc107"></i>
+                        <i class="fa fa-star-o" style="color:#ffc107"></i>
+                    </div>
                 </div>
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                     <div class="text-center"><a class="btn btn-outline-dark" href="prod?id=${produto.id}">Ver mais</a></div>
